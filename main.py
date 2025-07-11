fac90114c6c9af01ad3a70d85176113f5c58766b
ebertmx <ebertmx@gmail.com>
2025-07-11 04:19:31 +0000

    d

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ def hello_world():
     # Change the message below
     return "Hello from GitHub! The trigger works!"
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
