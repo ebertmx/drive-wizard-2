01ef24ad4ce4e009833738025208c3c916245c95
ebertmx <ebertmx@gmail.com>
2025-07-11 04:10:34 +0000

    test: a change to test the ci/cd trigger

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    # Change the message below
+    return "Hello from GitHub! The trigger works!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
