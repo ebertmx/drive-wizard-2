aa90aad5311f1cc146994926558382073602d567
ebertmx <ebertmx@gmail.com>
2025-07-16 05:17:53 +0000

    test

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello():
     return "✅ Hello from drive-api-hello via GitHub & Cloud Run!"
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
