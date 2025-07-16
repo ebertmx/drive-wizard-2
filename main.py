bdd105e2ba4f2540e2c1ba356f3e67d6f939930a
Matthew Ebert <87283949+ebertmx@users.noreply.github.com>
2025-07-15 22:08:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    # Change the message below
-    return "Hello from GitHub! The trigger works!"
-
+def hello():
+    return "✅ Hello from drive-api-hello via GitHub & Cloud Run!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
